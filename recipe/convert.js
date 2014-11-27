7aaa95af6cc2c40348f7165a7316f30d49f56ec2
Brett Stahlman <brett.p.stahlman@saic.com>
2014-11-26 20:07:05 -0600

    Got things refactored and somewhat tested. Perhaps a bit of cleanup needed when not riding in car...

--- recipe/convert.js
+++ recipe/convert.js
@@ -4,35 +4,37 @@ function Fraction(whole, numer, denom, scaleNumer, scaleDenom) {
     this.w = undefined;
     this.n = scaleNumer * (whole * denom + numer);
     this.d = scaleDenom * denom;
-    this.approx = null;
-    this.floatVal = this.w + this.n / this.d;
     // Will be set false if exact denom not nice.
     this.isNice = true;
-    // If non-nice fraction can be approximated by one with "nice" denom, pctErr represents the % error.
+    // If non-nice fraction is approximated by one with "nice" denom, pctErr will be set to the % error.
     this.pctErr = undefined;
     this.normalize();
 }
 Fraction.prototype = {
     niceDenoms: [2, 3, 4, 5, 8],
     acceptablePctErr: 5,
+    // Convert a simple fraction to the even simpler whole number form if possible, returning true if conversion
+    // performed.
     convertSpecial: function() {
         if (this.n === this.d) {
             this.w = 1;
-            this.n = this.d = undefined;
-            return true;
         } else if (this.d === 1) {
             this.w = this.n;
-            this.n = this.d = undefined;
-            return true;
         } else if (this.n % this.d === 0) {
             this.w = this.n / this.d;
-            this.n = this.d = undefined;
         } else if (!this.n) {
             this.w = 0;
-            this.n = this.d = undefined;
+        } else {
+            // No special conversion possible.
+            return false;
         }
-
+        // Will get here only if special conversion performed.
+        this.n = this.d = undefined;
+        return true;
     },
+    // Normalize the scaled fraction.
+    // Input is in simple form, but output should be in mixed or simple whole number form, approximated to "nice"
+    // denominator if possible.
     normalize: function() {
         // Reduce the (potentially improper) fraction if possible.
         var gcf = this.gcf(this.n, this.d);
@@ -46,14 +48,14 @@ Fraction.prototype = {
             if (this.niceDenoms.indexOf(this.d) === -1) {
                 this.isNice = false;
                 if (this.approximate()) {
-                    // We were able to approx; make sure approximate fraction isn't special: e.g.,
+                    // We were able to approximate; make sure approx fraction isn't special: e.g.,
                     // 15/16 becomes 16/16
                     // 17/4 becomes 16/4
                     this.convertSpecial();
                 }
             }
             // Even if approximation failed, convert back to proper mixed form (unless convertSpecial has already
-            // produced special form admitting no further simplification).
+            // produced whole number form admitting no further simplification).
             if (this.d && this.n > this.d) {
                 // Improper fraction.
                 this.w = Math.floor(this.n / this.d);
@@ -61,6 +63,9 @@ Fraction.prototype = {
             }
         }
     },
+    // Convert an ugly fraction to a nice one if we can do so without introducing too much error.
+    // Input: Non-nice simple fraction, possibly improper.
+    // Return: true indicates approximation performed, in which case this.pctErr gives error.
     approximate: function() {
         var denoms = this.niceDenoms,
             ln = denoms.length;
@@ -92,23 +97,19 @@ Fraction.prototype = {
         return ns[1];
     },
     toString: function() {
-        // Cases:
-        // --Fraction--
-        // !nice but have approx
-        // nice
-        //
         if (this.isNice || this.pctErr !== undefined) {
             // Use fraction
             if (this.n === undefined) {
                 // Pure integer - not mixed number
                 return this.w + "";
             } else {
-                // (Potentially-mixed) fraction
+                // Mixed fraction (possibly with 0 whole number part)
                 return (this.w ? this.w + " ": "") + this.n + "/" + this.d;
             }
         } else {
             // Use float
-            return Number(value).toFixed(1).replace(/\.0+$/, '');
+            var value = (this.w || 0) + (this.n || 0) / (this.d || 1);
+            return value.toFixed(1).replace(/\.0+$/, '');
         }
     }
 };
@@ -149,52 +150,21 @@ Scaler.prototype = {
         var me = this,
             factor = newCnt / oldCnt;
         return s.replace(this.regex, function(match, p1, p2, p3, p4, p5, p6) {
-            var value;
             // Note: Empirically, I've found that capturing groups matching nothing are undefined, but I don't want to
             // rely upon this as I haven't seen it documented.
             if (!!p2) {
-                value = newCnt * Number(p2) / oldCnt;
+                var value = newCnt * p2 / oldCnt;
+                // Scale the value and round the result to a single decimal place, removing any trailing ".0" for
+                // aesthetic reasons.
+                return value.toFixed(1).replace(/\.0+$/, '') + ' ' + p6;
             } else {
                 // Try to keep as (potentially mixed) fraction (i.e., avoid conversion to float)
                 var w = Number(p1 || p3 || 0),
                     n = Number(p4 || 0),
-                    d = Number(p5 || 1);
-                // Convert mixed number to (potentially improper) fraction.
-                var n = w * d + n;
-                // Scale it by newCnt/oldCnt, also as (potentially improper) fraction
-                n *= newCnt;
-                d *= oldCnt;
-                // Reduce the fraction if possible.
-                var gcf = me.gcf(n, d);
-                if (gcf > 1) {
-                    n /= gcf;
-                    d /= gcf;
-                }
-                // TODO: Do the following only for certain nice denominators; else convert to float val and
-                // fall-through...
-                // Convert to (potentially) mixed number.
-                if (n === d) {
-                    value = 1;
-                } else if (d === 1) {
-                    value = n;
-                } else {
-                    // TODO: Leave as float if denom not nice...
-                    if (n > d) {
-                        // Improper fraction.
-                        // !!!!UNDER CONSTRUCTION!!!!
-                        w = Math.floor(n / d);
-                        n = n % d;
-                        value = w + " " + n + "/" + d;
-                    } else {
-                        value = n + "/" + d;
-                    }
-                    return value + ' ' + p6;
-                }
+                    d = Number(p5 || 1),
+                    fraction = new Fraction(w, n, d, newCnt, oldCnt);
+                return fraction.toString() + ' ' + p6;
             }
-            // If we get here, couldn't or didn't want to display mixed fraction.
-            // Scale the value and round the result to a single decimal place, removing a trailing ".0" for aesthetic
-            // reasons.
-            return Number(value).toFixed(1).replace(/\.0+$/, '') + ' ' + p6;
         });
     }
 };
